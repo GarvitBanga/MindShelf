2c54dcc3f2ff193993807cd173d70659c57839ff
Garvit Banga <bangaboy181@gmail.com>
2025-03-28 01:20:23 -0400

    frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,14 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "./components/ui/Button";
 import { PlusIcon } from './icons/PlusIcon';
+import { ShareIcon } from './icons/ShareIcon';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Button startIcon={<PlusIcon size='md'/>} variant="primary" size="sm" text="Button" onClick={()=>{alert("clicked")}}/>
-      <Button variant="secondary" size="sm" text="Secondary" onClick={()=>{alert("clicked")}}/>
-    </div>
+    <>
+      <Button startIcon={<PlusIcon size='lg'/>} endIcon={<ShareIcon size='lg'/>} variant="primary" size="lg" text="Share" onClick={()=>{alert("clicked")}}/>
+      <Button startIcon={<PlusIcon size='lg'/>} endIcon={<ShareIcon size='lg'/>} variant="secondary" size="lg" text="Sharesec" onClick={()=>{alert("clicked")}}/>
+      <Button startIcon={<PlusIcon size='sm'/>} endIcon={<ShareIcon size='sm'/>} variant="secondary" size="sm" text="share" onClick={()=>{alert("clicked")}}/>
+      <Button startIcon={<PlusIcon size='md'/>} endIcon={<ShareIcon size='md'/>} variant="secondary" size="md" text="share" onClick={()=>{alert("clicked")}}/>
+
+      {/* <PlusIcon size="sm"/>
+      <PlusIcon size="md"/>
+      <PlusIcon size="lg"/> */}
+    </>
   )
 }
 
--- Frontend/src/components/ui/Button.tsx
+++ Frontend/src/components/ui/Button.tsx
@@ -14,14 +14,20 @@ const variantStyles={
     "secondary":"bg-purple-300 text-purple-600",
 }
 const sizeStyles={
-    "sm":"py-1 px-2",
-    "md":"py-2 px-4",
-    "lg":"py-4 px-6",
+    "sm":"py-1 px-2 rounded-sm",
+    "md":"py-2 px-4 rounded-md",
+    "lg":"py-4 px-8 rounded-xl ",
 }
 const defaultStyles="rounded-md flex";
 
 export const Button=(props:ButtonProps)=>{
-   return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]} `}>{props.startIcon?<div className="pr-2">{props.startIcon}</div>:null}{props.text}{props.endIcon?<div className="pl-2">{props.endIcon}</div>:""}</button>
+   return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]} `}>
+    <div className="flex items-center justify-center"> 
+        {props.startIcon?<div className="pr-2">{props.startIcon}</div>:null}
+        {props.text}
+        {props.endIcon?<div className="pl-2">{props.endIcon}</div>:""}
+        </div>
+    </button>
 
 
 }
